5a7c0d7a2d8bc937b6e608752fda35559d29fc49
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-22 15:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function onloadFunc() {
+	body.innerText = "DOM load success";
+}
+let body = document.getElementsByTagName("body")[0];
+body.onload = onloadFunc();
